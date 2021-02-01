13c7c817c50442971cb7477bb00f346489b34809
warmoverdrive <sweetsthebear@gmail.com>
2021-02-01 14:10:55 -0800

    # Unit spawning Set up basic unit spawning mechanic by clicking on a "spawn building" Decoupled player from unit, but still allowing the unit control from the mouse Set up authority for units/spawners player ownership

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,5 +5,17 @@ using Mirror;
 
 public class RTSNetworkManager : NetworkManager
 {
-    
+	[SerializeField] private GameObject unitSpawnerPrefab = null;
+
+	public override void OnServerAddPlayer(NetworkConnection conn)
+	{
+		base.OnServerAddPlayer(conn);
+
+		GameObject unitSpawnerInstance = Instantiate(
+			unitSpawnerPrefab, 
+			conn.identity.transform.position, 
+			conn.identity.transform.rotation);
+
+		NetworkServer.Spawn(unitSpawnerInstance, conn);
+	}
 }
