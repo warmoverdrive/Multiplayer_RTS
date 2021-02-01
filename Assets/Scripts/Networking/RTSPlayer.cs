0fa8539766c269cb390272ef26b5657aecf0dc48
warmoverdrive <sweetsthebear@gmail.com>
2021-02-01 15:23:20 -0800

    # Player Ownership - Set up player ownership of units by tracking them in a list

---
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -0,0 +1,83 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSPlayer : NetworkBehaviour
+{
+    [SerializeField] private List<Unit> myUnits = new List<Unit>();
+
+	#region Server
+
+	public override void OnStartServer()
+	{
+		base.OnStartServer();
+
+		// Subscribing to events and hooking them to local methods
+		Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
+		Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned;
+	}
+
+	public override void OnStopServer()
+	{
+		base.OnStopServer();
+
+		// unsubscribing to events and removing hooks
+		Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
+		Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned;
+	}
+
+	private void ServerHandleUnitSpawned(Unit unit)
+	{
+		// check that unit is owned by this player (by checking connection ID)
+		if (unit.connectionToClient.connectionId != connectionToClient.connectionId)
+			return;
+		myUnits.Add(unit);
+	}
+
+	private void ServerHandleUnitDespawned(Unit unit)
+	{
+		// check that unit is owned by this player (by checking connection ID)
+		if (unit.connectionToClient.connectionId != connectionToClient.connectionId)
+			return;
+		myUnits.Remove(unit);
+	}
+
+	#endregion
+
+	#region Client
+
+	public override void OnStartClient()
+	{
+		base.OnStartClient();
+		if (!isClientOnly)
+			return;
+		Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned;
+		Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitDespawned;
+	}
+
+	public override void OnStopClient()
+	{
+		base.OnStopClient();
+		if (!isClientOnly)
+			return;
+		Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned;
+		Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitDespawned;
+	}
+
+	private void AuthorityHandleUnitSpawned(Unit unit)
+	{
+		if (!hasAuthority)
+			return;
+		myUnits.Add(unit);
+	}
+
+	private void AuthorityHandleUnitDespawned(Unit unit)
+	{
+		if (!hasAuthority)
+			return;
+		myUnits.Remove(unit);
+	}
+
+	#endregion
+}
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using UnityEngine.Events;
+using System;
 
 public class Unit : NetworkBehaviour
 {
@@ -10,12 +11,54 @@ public class Unit : NetworkBehaviour
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeselected = null;
 
+	public static event Action<Unit> ServerOnUnitSpawned;
+	public static event Action<Unit> ServerOnUnitDespawned;
+	public static event Action<Unit> AuthorityOnUnitSpawned;
+	public static event Action<Unit> AuthorityOnUnitDespawned;
+
+
 	public UnitMovement GetUnitMovement()
 	{
 		return unitMovement;
 	}
 
+	#region Server
+
+	public override void OnStartServer()
+	{
+		base.OnStartServer();
+		ServerOnUnitSpawned?.Invoke(this);
+	}
+
+	public override void OnStopServer()
+	{
+		base.OnStopServer();
+		ServerOnUnitDespawned?.Invoke(this);
+	}
+
+	#endregion
+
 	#region Client
+
+	public override void OnStartClient()
+	{
+		base.OnStartClient();
+
+		if (!isClientOnly || !hasAuthority)
+			return;
+
+		AuthorityOnUnitSpawned?.Invoke(this);
+	}
+
+	public override void OnStopClient()
+	{
+		base.OnStopClient();
+
+		if (!isClientOnly || !hasAuthority)
+			return;
+
+		AuthorityOnUnitDespawned?.Invoke(this);
+	}
 
 	[Client]
 	public void Select()
