11d619d7a0aad7d3451fc88cbaa7e2d8f38ecec2
warmoverdrive <sweetsthebear@gmail.com>
2021-02-01 14:46:42 -0800

    # Mouse selection -Set up basic mouse selection for units - TODO implement giving orders to only selected units

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] private UnityEvent onSelected = null;
+    [SerializeField] private UnityEvent onDeselected = null;
+
+	#region Client
+
+	[Client]
+	public void Select()
+	{
+		if (!hasAuthority)
+			return;
+		onSelected?.Invoke();
+	}
+
+	[Client]
+	public void Deselect()
+	{
+		if (!hasAuthority)
+			return;
+		onDeselected?.Invoke();
+	}
+
+	#endregion
+
+}
